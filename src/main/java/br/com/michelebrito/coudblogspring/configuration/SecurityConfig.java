93c6e032372b523cbd3539959cc219daef0a3c6a
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-08-06 17:25:30 -0300

    Implementação do Spring Security e Pagina de login

---
+++ src/main/java/br/com/michelebrito/coudblogspring/configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.michelebrito.coudblogspring.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_LIST = { "/", "/posts", "/posts/{id}" };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers(AUTH_LIST).permitAll().anyRequest().authenticated().and()
+                .formLogin().permitAll().and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("mtz").password("{noop}123").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //Caso eu baixe o codigo da bootstrap tenho q p liberar, passando o caminho
+        web.ignoring().antMatchers("/bootstrap/**");
+        // web.ignoring().antMatchers("/bootstrap/**", "/style/**");
+    }
+}
